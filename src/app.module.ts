b074edafc17bbac84f25e20d1102cf71b36ea1c5
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-09-07 22:59:35 -0500

    config - controllers / services

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, MongooseModule.forRoot('mongodb://localhost/nest-crud-project')],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, Req, Body, HttpStatus } from '@nestjs/common';
+import { CreateProductDTO } from "./dto/product.dto";
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+
+  @Post('/create')
+  createProduct(@Res() res, @Req() req, @Body() createProductDTO: CreateProductDTO) {
+    //console.log(createProductDTO)
+    
+    return res.status(HttpStatus.OK).json({message: "received"})
+  }
+
+
+
+}
